d7b9513f0d59bcad449ed2fe32b6c510139cbf9c
Juan Jose Fernandez Duarte <juanjofer1999@gmail.com>
2020-07-08 17:44:34 -0400

    estilos y bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Badge from "./components/Badge";
-
+import "bootstrap/dist/css/bootstrap.css";
 const container = document.getElementById("root");
 
 ReactDOM.render(<Badge />, container);
