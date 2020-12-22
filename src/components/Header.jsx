0e65d2fb622978b23d36c5d11b5c7436ee934223
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-12-21 20:37:46 -0400

    sticky navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,80 +1,81 @@
 import React from "react";
 import "./../styles/Navbar.scss";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => (
-	<header>
-		<div className="Navbar navbar navbar-expand-lg">
-			<div className="container justify-content-md-center">
-				<div className="row collapse navbar-collapse" id="navbarSupportedContent">
-					<div className="colnavbar-brand">
-						<a href="">
-							<img src="src/assets/images/logo.png" alt="jjfd_logo" width="50" height="50" />
-						</a>
-					</div>
-					<ul className="col navbar-nav mr-auto">
-						<li className="nav-item active">
-							<a className="nav-link navlinks" href="">
-								HOME
-							</a>
-						</li>
-						<li className="nav-item active">
-							<a className="nav-link navlinks" href="">
-								PROFILE
-							</a>
-						</li>
-						<li className="nav-item active">
-							<a className="nav-link navlinks" href="">
-								EXPIRIENCES
-							</a>
-						</li>
-						<li className="nav-item active">
-							<a className="nav-link navlinks" href="">
-								CONTACT
-							</a>
-						</li>
-					</ul>
-					<ul className="col-sm-4 navbar-nav mr-auto">
-						<li className="nav-item active">
-							<a className="nav-link navlinks" href="">
-								BLOG
-							</a>
-						</li>
-						<li className="nav-item active">
-							<a className="nav-link navlinks" href="">
-								MY COURSES
-							</a>
-						</li>
-					</ul>
-					<ul className="col col-lg-2 navbar-nav mr-auto">
-						<li className="nav-item active">
-							<a className="nav-link" href="https://www.instagram.com/juanjo53fd/" target="_blank">
-								<img src="src/assets/images/instagram-white.png" alt="github" width="30" height="30" />
-							</a>
-						</li>
-						<li className="nav-item active">
-							<a className="nav-link" href="https://twitter.com/JuanJo53FD" target="_blank">
-								<img src="src/assets/images/twitter-white.png" alt="github" width="30" height="30" />
-							</a>
-						</li>
-						<li className="nav-item active">
-							<a
-								className="nav-link "
-								href="https://www.linkedin.com/in/juan-josé-fernández-duarte-096274163"
-								target="_blank"
-							>
-								<img src="src/assets/images/linkedin-white.png" alt="github" width="30" height="30" />
-							</a>
-						</li>
-						<li className="nav-item active">
-							<a className="nav-link" href="https://github.com/JuanJo53" target="_blank">
-								<img src="src/assets/images/github-white.png" alt="github" width="30" height="30" />
-							</a>
-						</li>
-					</ul>
+	// <header>
+	<div className="Navbar sticky-top navbar navbar-expand-lg">
+		<div className="container justify-content-md-center">
+			<div className="row collapse navbar-collapse" id="navbarSupportedContent">
+				<div className="colnavbar-brand">
+					<a href="">
+						<img src="src/assets/images/logo.png" alt="jjfd_logo" width="50" height="50" />
+					</a>
 				</div>
+				<ul className="col navbar-nav mr-auto">
+					<li className="nav-item ">
+						<a className="nav-link navlinks active" aria-current="page" href="#">
+							HOME
+						</a>
+					</li>
+					<li className="nav-item active">
+						<a className="nav-link navlinks" href="">
+							PROFILE
+						</a>
+					</li>
+					<li className="nav-item active">
+						<a className="nav-link navlinks" href="">
+							EXPIRIENCES
+						</a>
+					</li>
+					<li className="nav-item active">
+						<a className="nav-link navlinks" href="">
+							CONTACT
+						</a>
+					</li>
+				</ul>
+				<ul className="col-sm-4 navbar-nav mr-auto">
+					<li className="nav-item active">
+						<a className="nav-link navlinks" href="">
+							BLOG
+						</a>
+					</li>
+					<li className="nav-item active">
+						<a className="nav-link navlinks" href="">
+							MY COURSES
+						</a>
+					</li>
+				</ul>
+				<ul className="col col-lg-2 navbar-nav mr-auto">
+					<li className="nav-item active">
+						<a className="nav-link" href="https://www.instagram.com/juanjo53fd/" target="_blank">
+							<img src="src/assets/images/instagram-white.png" alt="github" width="30" height="30" />
+						</a>
+					</li>
+					<li className="nav-item active">
+						<a className="nav-link" href="https://twitter.com/JuanJo53FD" target="_blank">
+							<img src="src/assets/images/twitter-white.png" alt="github" width="30" height="30" />
+						</a>
+					</li>
+					<li className="nav-item active">
+						<a
+							className="nav-link "
+							href="https://www.linkedin.com/in/juan-josé-fernández-duarte-096274163"
+							target="_blank"
+						>
+							<img src="src/assets/images/linkedin-white.png" alt="github" width="30" height="30" />
+						</a>
+					</li>
+					<li className="nav-item active">
+						<a className="nav-link" href="https://github.com/JuanJo53" target="_blank">
+							<img src="src/assets/images/github-white.png" alt="github" width="30" height="30" />
+						</a>
+					</li>
+				</ul>
 			</div>
 		</div>
-	</header>
+	</div>
+	// </header>
 );
 
 export default Header;
