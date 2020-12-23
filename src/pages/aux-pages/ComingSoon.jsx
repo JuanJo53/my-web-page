b12a619ab27736cd1ca50d69f8acbbf96b58f1ef
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-12-23 17:39:04 -0400

    responsive content on coming soon page

--- src/pages/aux-pages/ComingSoon.jsx
+++ src/pages/aux-pages/ComingSoon.jsx
@@ -11,9 +11,9 @@ class ComingSoon extends Component {
 	render() {
 		const { countdown } = this.state;
 		return (
-			<div className="comingsoon container ">
+			<div className="comingsoon container-fluid container-lg">
 				<div className="row align-items-center">
-					<div className="time col-6">
+					<div className="time col col-sm-auto col-lg-4 col-lg-6 col-md-auto">
 						<div className="row">
 							<h1 className="fs-1 fw-bolder">Time Remaining!</h1>
 						</div>
@@ -24,7 +24,7 @@ class ComingSoon extends Component {
 							<button className="btn btn-primary disabled">NOTIFY ME!</button>
 						</div>
 					</div>
-					<div className="coming col">
+					<div className="coming col col-sm-auto col-lg-4 col-lg-5 col-md-auto ">
 						<h1 className="fs-1 fw-bolder">This Page is Coming Soon!</h1>
 						<h5 className="fw-normal">
 							This sections is still in building. It will be available for your soon, with some awesome stuff to
