400c061eecac3558b1b417366faee9b9dc079abc
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-12-24 20:01:06 -0400

    starting experiences section

--- src/pages/home/Experiences.js
+++ src/pages/home/Experiences.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
+import "../../styles/Experiences.scss";
 export default class Experiences extends Component {
 	render() {
 		return (
 			<div id="experiences" className="container experiences-container">
-				Soy el expericencias
+				<div className="experiences-header">
+					<h1 className="experiences-title fw-bolder">Experiences</h1>
+					<h5 className="experiences-desc">Here´s some of my work and experiences.</h5>
+					<hr className="solid"></hr>
+				</div>
 			</div>
 		);
 	}
