3d1ef1af5d6e51f8eb7fa6fc8de69ba9eb940b77
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-12-30 15:46:00 -0400

    profile responsive (v1)

--- src/pages/home/Profile.js
+++ src/pages/home/Profile.js
@@ -12,12 +12,12 @@ class Home extends React.Component {
 					<h5 className="profile-desc">I'm a software developer</h5>
 					<hr className="solid"></hr>
 				</div>
-				<Card className="card container profile-card">
-					<Card.Body className="row justify-content-md-center card-body profile-card-body">
-						<div className="col col-4 profile-img">
-							<Card.Img src="./src/assets/images/profile.jpg" />
+				<Card className="card container profile-card text-sm-center text-lg-left text-center">
+					<Card.Body className="row justify-content-md-center justify-content-center justify-content-sm-center card-body profile-card-body">
+						<div className="col-8 col-md-4 profile-img">
+							<Card.Img src="./src/assets/images/profile.jpg" className="img-fluid" />
 						</div>
-						<div className="col col-4 profile-details">
+						<div className="col-8 col-md-4 profile-details">
 							<Card.Title as="h2" className="profile-details-title">
 								Details
 							</Card.Title>
@@ -26,13 +26,13 @@ class Home extends React.Component {
 							<Card.Subtitle className="profile-details-subtitle">Age:</Card.Subtitle>
 							<Card.Text className="profile-details-text">21 years </Card.Text>
 							<Card.Subtitle className="profile-details-subtitle">Location:</Card.Subtitle>
-							<Card.Text className="profile-details-text">La Paz, Bolivia, Earth</Card.Text>
+							<Card.Text className="profile-details-text ">La Paz, Bolivia, Earth</Card.Text>
 						</div>
-						<div className="col col-4 profile-about">
+						<div className="col-8 col-md-4 profile-about">
 							<Card.Title as="h2" className="profile-about-title">
 								About me
 							</Card.Title>
-							<Card.Text className="profile-about-text">
+							<Card.Text className="profile-about-text ">
 								Systems engineer student. Lover of software development. Enthusiastic leader eager to improve
 								everything in his life. Passionate about never stopping learning.
 							</Card.Text>
