f30e4a8091ece4c976ddd88cfdb60cee430da8c0
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-12-30 17:44:43 -0400

    contact begining

--- src/pages/home/Contact.js
+++ src/pages/home/Contact.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 
+import "../../styles/Contact.scss";
 export default class Contact extends Component {
 	render() {
 		return (
 			<div id="contact" className="container contact-container">
-				Soy el contact
+				<div className="contact-header">
+					<h1 className="contact-title fw-bolder">Contact</h1>
+				</div>
 			</div>
 		);
 	}
