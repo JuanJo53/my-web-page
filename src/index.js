5dbfcd7ecc213ed5f50b5dddb7d7127604334912
Juan Jose Fernandez <juanjofer1999@gmail.com>
2021-01-01 19:38:46 -0400

    firebase config modified

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./containers/App";
 import "bootstrap/dist/css/bootstrap.css";
 import "../src/styles/global.css";
+
 import "./services/firebase-config";
 
 const container = document.getElementById("root");
